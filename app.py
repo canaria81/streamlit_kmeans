5c05a021acef3310e95eb92fc0229ec0bd692413
canaria81 <120348849+canaria81@users.noreply.github.com>
2022-12-16 12:40:35 +0900

    rev

--- app.py
+++ app.py
@@ -7,21 +7,21 @@ import matplotlib.pyplot as plt
 def main() :
     st.title('K-Means 클러스터링')
 
-    # 1. csv 파일을 업로드 할 수 있다.
+    # csv 파일을 업로드 할 수 있다.
     file = st.file_uploader('CSV파일 업로드', type=['csv'])
    
     if file is not None :
-        # 2. csv 파일은, 판다스로 읽어 화면에 표시한다.
+        # csv 파일은, 판다스로 읽어 화면에 표시한다.
         df = pd.read_csv(file)
         st.dataframe( df )
-        # 3.X로 사용할 컬럼을 설정할 수 있다.
+        # X로 사용할 컬럼을 설정할 수 있다.
         selected_columns = st.multiselect('X로 사용할 컬럼을 선택하세요', df.columns)
         if len(selected_columns) != 0 :
             X = df[selected_columns]
             st.dataframe(X)
 
             st.subheader('WCSS를 위한 클러스터링 개수 선택')
-            max_number = st.slider('최대 그룹 수 선택', 2, 20, value=10) # value = 유저가 선택 안했을 때 디폴트 값...
+            max_number = st.slider('최대 그룹 수 선택', 2, 20, value=10) # value = 디폴트 값
         
             wcss = []
             for k in np.arange(1, max_number+1) :
@@ -39,8 +39,8 @@ def main() :
             plt.ylabel('WCSS')
             st.pyplot(fig1)
 
-        # 실제로 그룹핑할 개수 선택!
-        # K = st.slider('그룹 개수 결정', 1, max_number) # 해보니까 로딩 시간이 많이 걸리더라..
+        # 실제 그룹핑할 개수 선택!
+        # K = st.slider('그룹 개수 결정', 1, max_number) # 로딩 시간이 많이 소요
             K = st.number_input('그룹 개수 결정', 1, max_number)
 
             kmeans = KMeans(n_clusters= K, random_state=5)
@@ -51,7 +51,7 @@ def main() :
 
             st.dataframe( df.sort_values('Group') )
 
-        # 데이터프레임은 이렇게 바로 파일로 저장하는 함수가 있음...
+        # 데이터프레임을 바로 파일로 저장하는 함수
             df.to_csv('result.csv')
 
 if __name__ == '__main__' :
